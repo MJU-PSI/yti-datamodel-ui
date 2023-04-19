4f20bd047af8acb7f2bd304e6e02d147d25e3fad
Tadej Šivak <tadej.sivak@gora.si>
2023-04-19 12:26:57 +0200

    angular BASE_HREF variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,13 +5,12 @@ set -e
 envsubst < /usr/share/nginx/html/configuration/configuration.template.js > /usr/share/nginx/html/configuration/configuration.js
 envsubst '${DATAMODEL_API_URL},${MESSAGING_API_URL}' < /etc/nginx/conf.d/nginx.template > /etc/nginx/conf.d/default.conf
 
-DATAMODEL_BASE_HREF=$ANGULAR_DATAMODEL_BASE_HREF
-if [[ $DATAMODEL_BASE_HREF != */ ]] # * is used for pattern matching
+if [[ $ANGULAR_BASE_HREF != */ ]] # * is used for pattern matching
 then
-  DATAMODEL_BASE_HREF="${DATAMODEL_BASE_HREF}/";
+  ANGULAR_BASE_HREF="${ANGULAR_BASE_HREF}/";
 fi
 
-sed -i 's#<base href="/">#<base href="'"${DATAMODEL_BASE_HREF}"'">#' /usr/share/nginx/html/index.html
+sed -i 's#<base href="/">#<base href="'"${ANGULAR_BASE_HREF}"'">#' /usr/share/nginx/html/index.html
 
 # Start nginx
 nginx -g 'daemon off;'
