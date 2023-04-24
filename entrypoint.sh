0b509f96b1ffbd17e9515bc1f27240608c1922f5
Tadej Šivak <tadej.sivak@gora.si>
2023-04-24 11:17:12 +0200

    setting base in index.html

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,12 +5,5 @@ set -e
 envsubst < /usr/share/nginx/html/configuration/configuration.template.js > /usr/share/nginx/html/configuration/configuration.js
 envsubst '${DATAMODEL_API_URL},${MESSAGING_API_URL}' < /etc/nginx/conf.d/nginx.template > /etc/nginx/conf.d/default.conf
 
-if [[ $ANGULAR_BASE_HREF != */ ]] # * is used for pattern matching
-then
-  ANGULAR_BASE_HREF="${ANGULAR_BASE_HREF}/";
-fi
-
-sed -i 's#<base href="/">#<base href="'"${ANGULAR_BASE_HREF}"'">#' /usr/share/nginx/html/index.html
-
 # Start nginx
 nginx -g 'daemon off;'
