ecb07d74d33a3cff39df49f23e51cabcd343e560
Aleš Bernik <ales.bernik@gora.si>
2023-11-13 12:49:16 +0100

    information about service

--- src/app/components/information/informationAboutServicePage.ts
+++ src/app/components/information/informationAboutServicePage.ts
@@ -1,13 +1,27 @@
+import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
 import { LocationService } from 'app/services/locationService';
 import { LegacyComponent } from 'app/utils/angular';
+import { Subscription } from 'rxjs';
 
 @LegacyComponent({
   template: require('./informationAboutServicePage.html')
 })
 export class InformationAboutServicePageComponent {
+  currentLang: String;
+  langChangeSubscription: Subscription;
 
-  constructor(locationService: LocationService) {
+  constructor(locationService: LocationService, translateService: TranslateService) {
     'ngInject';
     locationService.atInformationAboutService();
+
+    this.currentLang = translateService.currentLang;
+
+    this.langChangeSubscription = translateService.onLangChange.subscribe((event: LangChangeEvent) => {
+      this.currentLang = event.lang;
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.langChangeSubscription.unsubscribe();
   }
 }
